2ea74758f7d47ee13abef6501e35dbe19a4bd44b
jevitha1010 <jevithasena97@gmail.com>
2019-09-19 19:41:49 +0530

    i am fibonic series

--- src/jevitha/Gitsample.java
+++ src/jevitha/Gitsample.java
@@ -3,7 +3,19 @@ package jevitha;
 public class Gitsample {
 	public static void main(String arg[])
 	{
-		System.out.println("Jevitha");
+		int a=0,b=1;
+		
+		System.out.println(a);
+		System.out.println(b);
+		for (int i=2;i<=10;i++)
+		{
+			int c=a+b;
+			System.out.println(c);
+			a=b;
+			b=c;
+		}
+		
+		
 	}
 	}
 
